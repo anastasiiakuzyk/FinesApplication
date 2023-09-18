d3e4ad00d5f68f59f4e9baf3d4796aa89208be40
anastasiiakuzyk <kuzykanastasija@gmail.com>
2023-09-18 17:52:06 +0300

    Added CarService interface

--- src/main/kotlin/ua/anastasiia/finesapp/service/CarService.kt
+++ src/main/kotlin/ua/anastasiia/finesapp/service/CarService.kt
@@ -1,43 +1,12 @@
 package ua.anastasiia.finesapp.service
 
-import org.springframework.stereotype.Service
 import ua.anastasiia.finesapp.dto.CarRequest
 import ua.anastasiia.finesapp.dto.CarResponse
-import ua.anastasiia.finesapp.dto.toEntity
-import ua.anastasiia.finesapp.dto.toResponse
-import ua.anastasiia.finesapp.entity.Car
-import ua.anastasiia.finesapp.exception.CarIdNotFoundException
-import ua.anastasiia.finesapp.exception.CarPlateDuplicateException
-import ua.anastasiia.finesapp.exception.CarPlateNotFoundException
-import ua.anastasiia.finesapp.repository.CarRepository
 
-@Service
-class CarService(val carRepository: CarRepository) {
-    fun saveCar(carRequest: CarRequest): CarResponse {
-        return carRepository.findByPlate(carRequest.plate)?.let {
-            throw CarPlateDuplicateException(carRequest.plate)
-        } ?: carRepository.save(carRequest.toEntity()).toResponse()
-    }
-
-    fun getCarByPlate(plate: String): CarResponse =
-        carRepository.findByPlate(plate)?.toResponse() ?: throw CarPlateNotFoundException(plate)
-
-    fun getCarById(id: Long): CarResponse {
-        return carRepository.findById(id).orElseThrow { throw CarIdNotFoundException(id) }.toResponse()
-    }
-
-    fun getAllCars(): List<CarResponse> = carRepository.findAll().map { car: Car -> car.toResponse() }
-
-    fun updateCarById(carUpdated: CarRequest, id: Long): CarResponse {
-        getCarById(id).copy(
-            id = id,
-            plate = carUpdated.plate,
-            mark = carUpdated.mark,
-            model = carUpdated.model,
-            color = carUpdated.color
-        ).also {
-            carRepository.save(it.toEntity())
-            return it.toEntity().toResponse()
-        }
-    }
+interface CarService {
+    fun saveCar(carRequest: CarRequest): CarResponse
+    fun getCarByPlate(plate: String): CarResponse
+    fun getCarById(id: Long): CarResponse
+    fun getAllCars(): List<CarResponse>
+    fun updateCarById(carUpdated: CarRequest, id: Long): CarResponse
 }
