bd8d2c7b1e5bb385b43a688ac56bcd114a853aa2
Anastasiia Kuzyk <54268657+anastasiiakuzyk@users.noreply.github.com>
2023-09-19 09:40:16 +0300

    Added newlines to CarService
    Co-authored-by: Sergii Gnatiuk <116563306+ajax-gnatiuk-s@users.noreply.github.com>

--- src/main/kotlin/ua/anastasiia/finesapp/service/CarService.kt
+++ src/main/kotlin/ua/anastasiia/finesapp/service/CarService.kt
@@ -4,9 +4,14 @@ import ua.anastasiia.finesapp.dto.CarRequest
 import ua.anastasiia.finesapp.dto.CarResponse
 
 interface CarService {
+
     fun saveCar(carRequest: CarRequest): CarResponse
+    
     fun getCarByPlate(plate: String): CarResponse
+    
     fun getCarById(id: Long): CarResponse
+    
     fun getAllCars(): List<CarResponse>
+    
     fun updateCarById(carUpdated: CarRequest, id: Long): CarResponse
 }
