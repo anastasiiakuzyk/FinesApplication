651bc8d37248d862a22a3f4d30f0293b67458c73
Anastasiia Kuzyk <54268657+anastasiiakuzyk@users.noreply.github.com>
2023-09-19 09:40:05 +0300

    Added newlines to FineService
    Co-authored-by: Sergii Gnatiuk <116563306+ajax-gnatiuk-s@users.noreply.github.com>

--- src/main/kotlin/ua/anastasiia/finesapp/service/FineService.kt
+++ src/main/kotlin/ua/anastasiia/finesapp/service/FineService.kt
@@ -5,9 +5,14 @@ import ua.anastasiia.finesapp.dto.FineResponse
 import ua.anastasiia.finesapp.entity.Fine
 
 interface FineService {
+
     fun createFine(fineRequest: FineRequest): Fine
+    
     fun getAllFines(): List<FineResponse>
+    
     fun getFinesByPlate(plate: String): List<FineResponse>
+    
     fun addViolations(fineId: Long, violationIds: Array<Long>): FineResponse
+    
     fun getFineById(id: Long): FineResponse
 }
