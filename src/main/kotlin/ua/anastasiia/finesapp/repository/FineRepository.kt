6946d489832288b008b215ac0239e66878b0cf86
anastasiiakuzyk <kuzykanastasija@gmail.com>
2023-09-28 18:59:33 +0300

    Deleted MySQL entities

--- src/main/kotlin/ua/anastasiia/finesapp/repository/FineRepository.kt
+++
@@ -1,8 +0,0 @@
-package ua.anastasiia.finesapp.repository
-
-import org.springframework.data.jpa.repository.JpaRepository
-import ua.anastasiia.finesapp.entity.Fine
-
-interface FineRepository : JpaRepository<Fine, Long> {
-    fun findAllByCarPlate(plateString: String): List<Fine>
-}
