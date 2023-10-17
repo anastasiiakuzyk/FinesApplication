75aa2bc143ba2ea93f01e871ca3f817f091abb49
anastasiiakuzyk <kuzykanastasija@gmail.com>
2023-10-17 11:00:48 +0300

    Move constants to companion object

--- finesapp/src/main/kotlin/ua/anastasiia/finesapp/beanPostProcessor/fieldGeneration/RandomPlateGenerator.kt
+++ finesapp/src/main/kotlin/ua/anastasiia/finesapp/beanPostProcessor/fieldGeneration/RandomPlateGenerator.kt
@@ -1,18 +1,18 @@
 package ua.anastasiia.finesapp.beanPostProcessor.fieldGeneration
 
 class RandomPlateGenerator : RandomFieldGenerator {
-    private val uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    private val numbers = "0123456789"
-    private val numberOfLetters = 2
-
-    @Suppress("MagicNumber")
-    private val numberOfNumbers = 4
+    companion object {
+        private const val LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        private const val NUMBERS = "0123456789"
+        private const val NUMBER_OF_LETTERS = 2
+        private const val NUMBER_NUMBERS = 4
+    }
 
     override fun generate(): String {
         return StringBuilder().apply {
-            repeat(numberOfLetters) { append(uppercaseLetters.random()) }
-            repeat(numberOfNumbers) { append(numbers.random()) }
-            repeat(numberOfLetters) { append(uppercaseLetters.random()) }
+            repeat(NUMBER_OF_LETTERS) { append(LETTERS.random()) }
+            repeat(NUMBER_NUMBERS) { append(NUMBERS.random()) }
+            repeat(NUMBER_OF_LETTERS) { append(LETTERS.random()) }
         }.toString()
     }
 }
