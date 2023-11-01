326398b1bb0230caabcd7b8daa621fe97b9c9047
anastasiiakuzyk <kuzykanastasija@gmail.com>
2023-11-01 14:25:25 +0200

    Update nats tests to work with reactive items and to correspond test structure

--- finesapp/src/test/kotlin/ua/anastasiia/finesapp/DeleteViolationNatsControllerTest.kt
+++ finesapp/src/test/kotlin/ua/anastasiia/finesapp/DeleteViolationNatsControllerTest.kt
@@ -1,58 +1,71 @@
 package ua.anastasiia.finesapp
 
+import io.nats.client.Connection
 import org.bson.types.ObjectId
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNull
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import ua.anastasiia.finesapp.NatsTestUtils.getFineToSave
+import ua.anastasiia.finesapp.NatsTestUtils.getTrafficTicketToSave
+import ua.anastasiia.finesapp.NatsTestUtils.sendRequestAndParseResponse
 import ua.anastasiia.finesapp.dto.toProto
 import ua.anastasiia.finesapp.dto.toViolation
 import ua.anastasiia.finesapp.dto.toViolationType
 import ua.anastasiia.finesapp.input.reqreply.violation.DeleteViolationRequest
 import ua.anastasiia.finesapp.input.reqreply.violation.DeleteViolationResponse
 import ua.anastasiia.finesapp.output.pubsub.violation.ViolationDeletedEvent
+import ua.anastasiia.finesapp.repository.MongoFineRepository
 import java.time.Duration
 
-class DeleteViolationNatsControllerTest : NatsControllerTest() {
+@SpringBootTest
+@ActiveProfiles("test")
+class DeleteViolationNatsControllerTest {
+
+    @Autowired
+    lateinit var connection: Connection
+
+    @Autowired
+    lateinit var fineRepository: MongoFineRepository
 
     @Test
-    fun `verify violation deletion, fine update, and related event publication`() {
+    fun `should update fine and publish related event when violation is deleted`() {
+        // GIVEN
         val trafficTicketToSave = getTrafficTicketToSave()
-        val fineToSave = getFineToSaveGeneratedCarPlate().copy(trafficTickets = listOf(trafficTicketToSave))
-        val savedFine = fineRepository.saveFine(fineToSave)
-
+        val fineToSave = getFineToSave().copy(trafficTickets = listOf(trafficTicketToSave))
+        val savedFine = fineRepository.saveFine(fineToSave).block()
         val expectedTrafficTicket =
             trafficTicketToSave.copy(violations = listOf(1, 2).map { it.toViolationType().toViolation() })
-        val expectedFine = savedFine.copy(trafficTickets = listOf(expectedTrafficTicket)).toProto()
+        val expectedFine = savedFine!!.copy(trafficTickets = listOf(expectedTrafficTicket)).toProto()
+        val createdEvent = connection.subscribe(NatsSubject.Violation.deletedSubject(savedFine.car.plate))
+        val expectedResponse =
+            DeleteViolationResponse.newBuilder().apply { successBuilder.setFine(expectedFine) }.build()
+        val expectedEvent = ViolationDeletedEvent.newBuilder().setFine(expectedFine).build()
 
-        val createdEvent = connection.subscribe(
-            NatsSubject.Violation.deletedSubject(savedFine.car.plate)
-        )
-
-        val expectedResponse = DeleteViolationResponse
-            .newBuilder()
-            .apply { successBuilder.setFine(expectedFine) }
-            .build()
-
-        val request = DeleteViolationRequest.newBuilder()
-            .setCarPlate(fineToSave.car.plate)
-            .setTicketId(savedFine.trafficTickets[0].id.toHexString())
-            .setViolationId(3)
-            .build()
+        // WHEN
         val actualResponse = sendRequestAndParseResponse(
+            connection = connection,
             subject = NatsSubject.Violation.DELETE,
-            request = request,
+            request = DeleteViolationRequest.newBuilder()
+                .setCarPlate(fineToSave.car.plate)
+                .setTicketId(savedFine.trafficTickets[0].id.toHexString())
+                .setViolationId(3)
+                .build(),
             parser = DeleteViolationResponse::parseFrom
         )
-        assertEquals(expectedResponse, actualResponse)
-
-        val expectedEvent = ViolationDeletedEvent.newBuilder().setFine(expectedFine).build()
         val actualEvent = ViolationDeletedEvent.parseFrom(createdEvent.nextMessage(Duration.ofSeconds(10)).data)
+
+        // THEN
+        assertEquals(expectedResponse, actualResponse)
         assertEquals(expectedEvent, actualEvent)
     }
 
     @Test
-    fun `verify failure when deleting non-existent or invalid violation`() {
+    fun `should return failure response and not publish event when deleting non-existent or invalid violation`() {
+        // GIVEN
         val invalidViolationId = 9
         val objectId = ObjectId()
         val ticketId = objectId.toHexString()
@@ -62,22 +75,23 @@ class DeleteViolationNatsControllerTest : NatsControllerTest() {
             .setTicketId(ticketId)
             .setViolationId(invalidViolationId)
             .build()
+        val expectedResponse = DeleteViolationResponse.newBuilder().apply {
+            failureBuilder.trafficTicketWithViolationNotFoundErrorBuilder
+        }.build()
+        val createdEvent = connection.subscribe(NatsSubject.Violation.deletedSubject(carPlate))
 
-        val expectedResponse = DeleteViolationResponse
-            .newBuilder()
-            .apply { failureBuilder.trafficTicketWithViolationNotFoundErrorBuilder }
-            .build()
-
+        // WHEN
         val actualResponse = sendRequestAndParseResponse(
+            connection = connection,
             subject = NatsSubject.Violation.DELETE,
             request = deleteViolationRequest,
             parser = DeleteViolationResponse::parseFrom
         )
+        val actualEvent = createdEvent.nextMessage(Duration.ofSeconds(2))
+
+        // THEN
         assertTrue(actualResponse.hasFailure())
         assertEquals(expectedResponse, actualResponse)
-
-        val createdEvent = connection.subscribe(NatsSubject.Violation.deletedSubject(carPlate))
-        val event = createdEvent.nextMessage(Duration.ofSeconds(2))
-        assertNull(event, "No event should be published for invalid violation deletion.")
+        assertNull(actualEvent, "No event should be published for invalid violation deletion.")
     }
 }
--- finesapp/src/test/kotlin/ua/anastasiia/finesapp/GetAllCarsNatsControllerTest.kt
+++ finesapp/src/test/kotlin/ua/anastasiia/finesapp/GetAllCarsNatsControllerTest.kt
@@ -1,43 +1,81 @@
 package ua.anastasiia.finesapp
 
+import io.nats.client.Connection
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate
+import org.springframework.data.mongodb.core.dropCollection
+import org.springframework.test.context.ActiveProfiles
+import ua.anastasiia.finesapp.NatsTestUtils.getFineToSave
+import ua.anastasiia.finesapp.NatsTestUtils.sendRequestAndParseResponse
 import ua.anastasiia.finesapp.dto.response.toCar
 import ua.anastasiia.finesapp.dto.toProto
+import ua.anastasiia.finesapp.entity.MongoFine
 import ua.anastasiia.finesapp.input.reqreply.car.GetAllCarsRequest
 import ua.anastasiia.finesapp.input.reqreply.car.GetAllCarsResponse
+import ua.anastasiia.finesapp.repository.MongoFineRepository
 
-class GetAllCarsNatsControllerTest : NatsControllerTest() {
+@SpringBootTest
+@ActiveProfiles("test")
+class GetAllCarsNatsControllerTest {
+
+    @Autowired
+    lateinit var connection: Connection
+
+    @Autowired
+    lateinit var fineRepository: MongoFineRepository
+
+    @Autowired
+    lateinit var reactiveMongoTemplate: ReactiveMongoTemplate
 
     @Test
-    fun `verify retrieval of all cars with existing entries`() {
-        fineRepository.saveFines(listOf(getFineToSaveGeneratedCarPlate()))
-
+    fun `should return success result`() {
+        // GIVEN
+        fineRepository.saveFines(listOf(getFineToSave())).collectList().block()
         val expectedResponse = GetAllCarsResponse
             .newBuilder()
-            .apply { successBuilder.addAllCars(fineRepository.getAllCars().map { it.toCar().toProto() }) }
+            .apply {
+                successBuilder.addAllCars(
+                    fineRepository.getAllCars()
+                        .collectList()
+                        .block()!!
+                        .map { it.toCar().toProto() }
+                )
+            }
             .build()
 
+        // WHEN
         val actualResponse = sendRequestAndParseResponse(
+            connection = connection,
             subject = NatsSubject.Car.GET_ALL,
             request = GetAllCarsRequest.newBuilder().build(),
             parser = GetAllCarsResponse::parseFrom
         )
+
+        // THEN
         assertEquals(expectedResponse, actualResponse)
     }
 
     @Test
-    fun `verify handling of no available cars scenario`() {
+    fun `should return failure result when no available cars`() {
+        // GIVEN
+        reactiveMongoTemplate.dropCollection<MongoFine>().block()
         val expectedResponse = GetAllCarsResponse
             .newBuilder()
             .apply { failureBuilder.carsNotFoundErrorBuilder }
             .build()
 
+        // WHEN
         val actualResponse = sendRequestAndParseResponse(
+            connection = connection,
             subject = NatsSubject.Car.GET_ALL,
             request = GetAllCarsRequest.newBuilder().build(),
             parser = GetAllCarsResponse::parseFrom
         )
+
+        // THEN
         assertEquals(expectedResponse, actualResponse)
     }
 }
