f01030e122f17fb2d9b17b76ba7206a340f51335
anastasiiakuzyk <kuzykanastasija@gmail.com>
2023-11-01 14:27:28 +0200

    Update rest controllers to be reactive

--- finesapp/src/main/kotlin/ua/anastasiia/finesapp/controller/rest/FineController.kt
+++ finesapp/src/main/kotlin/ua/anastasiia/finesapp/controller/rest/FineController.kt
@@ -2,6 +2,7 @@ package ua.anastasiia.finesapp.controller.rest
 
 import jakarta.validation.Valid
 import org.bson.types.ObjectId
+import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -10,9 +11,10 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
 import ua.anastasiia.finesapp.dto.request.FineRequest
 import ua.anastasiia.finesapp.dto.response.FineResponse
-import ua.anastasiia.finesapp.entity.MongoFine
 import ua.anastasiia.finesapp.service.FineService
 import java.time.LocalDate
 
@@ -20,32 +22,32 @@ import java.time.LocalDate
 @RequestMapping(value = ["/fines"])
 class FineController(val fineService: FineService) {
 
-    @GetMapping
-    fun getAllFines(): List<FineResponse> = fineService.getAllFines()
+    @GetMapping(produces = [MediaType.TEXT_EVENT_STREAM_VALUE])
+    fun getAllFines(): Flux<FineResponse> = fineService.getAllFines()
 
     @GetMapping("location")
     fun getAllFinesInLocation(
         @RequestParam longitude: Double,
         @RequestParam latitude: Double,
         @RequestParam radius: Double
-    ): List<FineResponse> =
+    ): Flux<FineResponse> =
         fineService.getAllFinesInLocation(longitude, latitude, radius)
 
     @GetMapping("date/{date}")
-    fun getAllFinesByDate(@PathVariable date: LocalDate): List<FineResponse> =
+    fun getAllFinesByDate(@PathVariable date: LocalDate): Flux<FineResponse> =
         fineService.getAllFinesByDate(date)
 
     @GetMapping("fine/{fineId}")
-    fun getFineById(@PathVariable fineId: ObjectId): FineResponse =
+    fun getFineById(@PathVariable fineId: ObjectId): Mono<FineResponse> =
         fineService.getFineById(fineId)
 
     @PostMapping
-    fun saveFine(@Valid @RequestBody fineRequest: FineRequest): FineResponse = fineService.saveFine(fineRequest)
+    fun saveFine(@Valid @RequestBody fineRequest: FineRequest): Mono<FineResponse> = fineService.saveFine(fineRequest)
 
     @PostMapping("many")
-    fun saveFines(@Valid @RequestBody mongoFines: List<FineRequest>): List<FineResponse> =
+    fun saveFines(@Valid @RequestBody mongoFines: List<FineRequest>): Flux<FineResponse> =
         fineService.saveFines(mongoFines)
 
     @DeleteMapping("fine/{fineId}")
-    fun deleteFineById(@PathVariable fineId: ObjectId): FineResponse = fineService.deleteFineById(fineId)
+    fun deleteFineById(@PathVariable fineId: ObjectId): Mono<FineResponse> = fineService.deleteFineById(fineId)
 }
