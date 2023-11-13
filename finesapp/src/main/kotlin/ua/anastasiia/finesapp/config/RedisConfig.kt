d5b88a1de03cc04bccc4818183d454ffe81f5e47
anastasiiakuzyk <kuzykanastasija@gmail.com>
2023-11-13 18:18:28 +0200

    Add redis config

---
+++ finesapp/src/main/kotlin/ua/anastasiia/finesapp/config/RedisConfig.kt
@@ -0,0 +1,29 @@
+package ua.anastasiia.finesapp.config
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.cache.annotation.EnableCaching
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory
+import org.springframework.data.redis.core.ReactiveRedisTemplate
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer
+import org.springframework.data.redis.serializer.RedisSerializationContext
+import org.springframework.data.redis.serializer.StringRedisSerializer
+import ua.anastasiia.finesapp.domain.Fine
+
+@Configuration
+@EnableCaching
+class RedisConfig {
+    @Bean
+    fun reactiveRedisTemplate(
+        connectionFactory: ReactiveRedisConnectionFactory
+    ): ReactiveRedisTemplate<String, Fine> {
+        val objectMapper = ObjectMapper().findAndRegisterModules()
+        val serializer = Jackson2JsonRedisSerializer(objectMapper, Fine::class.java)
+        val context = RedisSerializationContext
+            .newSerializationContext<String, Fine>(StringRedisSerializer())
+            .value(serializer)
+            .build()
+        return ReactiveRedisTemplate(connectionFactory, context)
+    }
+}
