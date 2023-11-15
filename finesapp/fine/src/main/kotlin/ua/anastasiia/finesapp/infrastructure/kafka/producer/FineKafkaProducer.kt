c887b4b8aacaa180f2c95f64ddfdd705e3e9c845
anastasiiakuzyk <kuzykanastasija@gmail.com>
2023-11-15 20:08:33 +0200

    Add TrafficTicketAddedEventProducerOut port interface

--- finesapp/fine/src/main/kotlin/ua/anastasiia/finesapp/infrastructure/kafka/producer/FineKafkaProducer.kt
+++ finesapp/fine/src/main/kotlin/ua/anastasiia/finesapp/infrastructure/kafka/producer/FineKafkaProducer.kt
@@ -1,4 +1,4 @@
-package ua.anastasiia.finesapp.infrastructure.adapters.kafka
+package ua.anastasiia.finesapp.infrastructure.kafka.producer
 
 import org.apache.kafka.clients.producer.ProducerRecord
 import org.springframework.stereotype.Component
@@ -6,15 +6,19 @@ import reactor.kafka.sender.KafkaSender
 import reactor.kafka.sender.SenderRecord
 import reactor.kotlin.core.publisher.toMono
 import ua.anastasiia.finesapp.KafkaTopic
-import ua.anastasiia.finesapp.commonmodels.fine.Fine
+import ua.anastasiia.finesapp.application.port.output.TrafficTicketAddedEventProducerOut
+import ua.anastasiia.finesapp.domain.Fine
+import ua.anastasiia.finesapp.infrastructure.mapper.toProto
 import ua.anastasiia.finesapp.output.pubsub.trafficticket.TrafficTicketAddedEvent
 
 @Component
 class FineKafkaProducer(
     private val kafkaSender: KafkaSender<String, TrafficTicketAddedEvent>
-) {
-    fun produceNotification(protoFine: Fine, trafficTicketId: String) {
-        val event = TrafficTicketAddedEvent.newBuilder().setId(trafficTicketId).setFine(protoFine).build()
+) : TrafficTicketAddedEventProducerOut {
+    override fun sendEvent(fine: Fine, trafficTicketId: Fine.TrafficTicket) {
+        val event = TrafficTicketAddedEvent.newBuilder()
+            .setId(trafficTicketId.toProto().id)
+            .setFine(fine.toProto()).build()
         kafkaSender.send(
             SenderRecord.create(
                 ProducerRecord(
